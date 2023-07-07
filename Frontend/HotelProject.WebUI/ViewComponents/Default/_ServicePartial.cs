c6daa996ccf2668705ae0a64c7240843673ae3d0
EnesFevzi <enssfevzi@gmail.com>
2023-07-08 01:46:09 +0300

    Little changed

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -0,0 +1,27 @@
+﻿using HotelProject.WebUI.Dtos.Service;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _ServicePartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7024/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
